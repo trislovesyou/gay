604434334fecb21bd9240bdde41be28a503a2436
trislovesyou <alwaysaqt11@gmail.com>
2025-03-14 23:32:35 -0500

    Update gaty.lua

--- gaty.lua
+++ gaty.lua
@@ -25,7 +25,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
         ['Bezier'] = (true), --[[ -- ]]
         ['Sticky'] = (true), --[[ Single Target ]]
 
-        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.00850), ['Air'] = (0.00843) },
+        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.00910), ['Air'] = (0.00903) },
 
         ['Prediction'] = { ['Enabled'] = (false), ['Ground'] = (0.11), ['Air'] = (0.11) },
                                                             --[[ Basic, Advanced ]]
