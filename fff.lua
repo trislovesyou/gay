9818efd7d9692977fa5ed19415942bde0a8f8f01
trislovesyou <alwaysaqt11@gmail.com>
2025-03-17 18:43:16 -0500

    Update fff.lua

--- fff.lua
+++ fff.lua
@@ -25,7 +25,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
         ['Bezier'] = (true), --[[ -- ]]
         ['Sticky'] = (true), --[[ Single Target ]]
 
-        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.00900), ['Air'] = (0.00880) },
+        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.00880), ['Air'] = (0.00870) },
 
         ['Prediction'] = { ['false'] = (false), ['Ground'] = (0.119), ['Air'] = (0.119) },
                                                             --[[ Basic, Advanced ]]
