6af0caf48fce0ccc67047448db57cab8a79864c7
trislovesyou <alwaysaqt11@gmail.com>
2025-03-18 22:29:38 -0500

    Update lol.lua

--- lol.lua
+++ lol.lua
@@ -89,7 +89,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
     ["Fields"] = {
         ['AimAssistField'] = { ['Size'] = (500) },
         ['SilentAimField'] = { ['Size'] = (110) },
-        ['TriggerBotField'] = { ['Size'] = (3.5) }, --[[ For offbrands ]]
+        ['TriggerBotField'] = { ['Size'] = (2.5) }, --[[ For offbrands ]]
         ['SilentAimBoxField'] = { ['Visible'] = (false), ['Sync'] = (true), ['Width'] = (3), ['Height'] = (2.7) }
     },
 }
